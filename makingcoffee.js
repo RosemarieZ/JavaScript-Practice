6352188b3911db6e35199bf591b9b6eb46b59356
Rosemarie Zayas <53095590+RosemarieZ@users.noreply.github.com>
2019-09-03 09:00:51 -0400

    Add files via upload

---
+++ makingcoffee.js
@@ -0,0 +1,10 @@
+function makingcoffee(sugar,milk){
+	var intructions = "boiling water,";
+	intructions += " pour water into cup,";
+	intructions += " add coffee,";
+	intructions += " add " + sugar + " spoons of sugar,";
+	intructions += " add " + milk + " cups of milk";
+
+	return intructions;
+}
+console.log(makingcoffee(4,8));
